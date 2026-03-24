ea60f6c26803ef9d7bcfcbf58dc281f8524e07e5
owner <owner@localhost>
2026-03-24 10:04:31 +0900

    コメント追加

--- src/src/components/development/VueTest.vue
+++ src/src/components/development/VueTest.vue
@@ -1,23 +1,77 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 
-const text = ref<string>('');
+interface Props {
+  apiHost: string;
+}
 
+const props = defineProps<Props>();
+
+const author = ref<string>('');
+const content = ref<string>('');
+const comments = ref<any[]>();
+
+/** 送信処理 */
 const handleSubmit = async (e: Event) => {
   e.preventDefault();
 
   const data = {
-    text: text.value,
+    author: author.value,
+    content: content.value,
   };
 
   console.log(data);
+
+  const url = `${props.apiHost}/`;
+
+  const res = await axios.post(url, data);
+
+  console.log(res.data);
+
+  author.value = '';
+  content.value = '';
+
+  await setList();
 };
+
+/** リスト設定 */
+const setList = async () => {
+  console.log(props.apiHost);
+
+  const url = `${props.apiHost}/`;
+
+  const res = await axios.get(url);
+
+  console.log(res.data);
+
+  comments.value = res.data;
+};
+
+// 初期化時
+onMounted(async () => {
+  console.log('mounted');
+
+  await setList();
+});
 </script>
 
 <template>
-  <form @submit="handleSubmit" class="max-w-md mx-auto space-y-4">
-    <div>テキスト</div>
-    <textarea v-model="text" class="app-form-input"></textarea>
-    <button type="submit" class="app-btn-primary">確認</button>
-  </form>
+  <div class="max-w-md mx-auto space-y-10">
+    <form @submit="handleSubmit" class="space-y-4">
+      <div>名前</div>
+      <input type="text" v-model="author" class="app-form-input" required />
+      <div>内容</div>
+      <textarea v-model="content" class="app-form-input" required></textarea>
+      <button type="submit" class="app-btn-primary">送信</button>
+    </form>
+
+    <div class="space-y-2">
+      <div class="border-2 p-5" v-for="comment in comments" :key="comment.id">
+        <div>{{ comment.id }}</div>
+        <div>{{ comment.author }}</div>
+        <div>{{ comment.content }}</div>
+      </div>
+    </div>
+  </div>
 </template>
